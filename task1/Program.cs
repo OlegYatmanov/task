dc700e384863d7013345368e1ba73e903164b463
Oleg Yatmanov <o_yatmanov@mail.ru>
2023-07-02 13:55:34 +0500

    Задача 2

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+
+if(B * B == A)
+{
+    Console.WriteLine("А является квадратом B");
+}
+else
+{
+   Console.WriteLine("А не является квадратом B"); 
+}
